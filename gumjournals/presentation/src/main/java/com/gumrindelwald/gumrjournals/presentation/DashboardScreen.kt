4fd4d9d21fe3aba1d7b19e094aaac76b8ffe9bce
gamalielara <aragamalielboanerges@gmail.com>
2026-01-21 09:08:31 +0800

    init gumjournal screen

--- gumjournals/presentation/src/main/java/com/gumrindelwald/gumrjournals/presentation/DashboardScreen.kt
+++ gumjournals/presentation/src/main/java/com/gumrindelwald/gumrjournals/presentation/DashboardScreen.kt
@@ -3,19 +3,31 @@ package com.gumrindelwald.gumjournals.presentation
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.BasicAlertDialog
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.res.vectorResource
 import androidx.compose.ui.tooling.preview.Preview
@@ -25,86 +37,106 @@ import com.gumrindelwald.designsystem.AppFont
 import com.gumrindelwald.designsystem.GumAppTheme
 import com.gumrindelwald.gumapp.core.presentation.designsystem.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun GumjournalsDashboardScreen() {
-    Column(
-        verticalArrangement = Arrangement.Center,
-        horizontalAlignment = Alignment.CenterHorizontally,
+    val interactionSource = remember { MutableInteractionSource() }
+    var showDialog by remember { mutableStateOf(false) }
+
+    Column (
         modifier = Modifier
             .fillMaxSize()
-            .background(MaterialTheme.colorScheme.primaryContainer)
+            .background(MaterialTheme.colorScheme.primaryContainer),
+        verticalArrangement = Arrangement.Center,
     ) {
-        Text(
-            "This is GumApp!",
-            fontFamily = AppFont,
-            fontSize = 20.sp,
-            color = MaterialTheme.colorScheme.onPrimaryContainer
-        )
-        Row(
-            horizontalArrangement = Arrangement.SpaceEvenly,
+        Column(
+            verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 10.dp)
+                .padding(10.dp)
+                .clip(RoundedCornerShape(20.dp))
+                .background(Color.White)
         ) {
-
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_weary),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
-                    .clickable(true, onClick = {})
-
-
+            Text(
+                "How was your day?",
+                fontFamily = AppFont,
+                fontSize = 18.sp,
+                color = MaterialTheme.colorScheme.onPrimaryContainer,
+                modifier = Modifier.padding(vertical = 10.dp)
             )
-
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_sorrow),
-                contentDescription = null,
+            Row(
+                horizontalArrangement = Arrangement.SpaceEvenly,
                 modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
+                    .fillMaxWidth()
+                    .padding(vertical = 10.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_weary),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                        .clickable(
+                            interactionSource, indication = null, onClick = { showDialog = true })
+                )
 
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_sorrow),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_flat),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_relieved),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_estatic_hugging),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                )
+                Image(
+                    imageVector = ImageVector.vectorResource(R.drawable.emoji_happy),
+                    contentDescription = null,
+                    modifier = Modifier
+                        .width(40.dp)
+                        .aspectRatio(1f)
+                )
+            }
 
-            )
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_flat),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
+            if (showDialog) {
+                BasicAlertDialog(
+                    onDismissRequest = { showDialog = false },
+                ) {
+                    Box(
+                        modifier = Modifier
+                            .width(100.dp)
+                            .aspectRatio(1f)
+                            .clip(
+                                shape = RoundedCornerShape(10.dp),
+                            )
+                            .background(MaterialTheme.colorScheme.tertiaryContainer)
+                            .padding(20.dp)
 
-
-            )
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_relieved),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
-
-
-            )
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_estatic_hugging),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
-
-
-            )
-            Image(
-                imageVector = ImageVector.vectorResource(R.drawable.emoji_happy),
-                contentDescription = null,
-                modifier = Modifier
-                    .width(60.dp)
-                    .aspectRatio(1f)
-
-
-            )
-
-
+                    ) {
+                        Text("Hello World")
+                    }
+                }
+            }
         }
     }
 }
